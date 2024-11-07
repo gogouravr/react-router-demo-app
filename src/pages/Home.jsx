4ce5d303674d9d5840211162a97f1f7e29b02f99
gouravrajbit <gouravr244@gmail.com>
2024-11-07 21:07:36 +0530

    Navigate route programatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Home() {
+    const navigate = useNavigate();
+
+    const clickHandler = () => {
+        navigate('products');
+    }
     return (
-        <div>Home</div>
+        <>
+            <div>Home</div>
+            <button onClick={clickHandler}>
+                Browse Products
+            </button>
+        </>
+
     )
 }
