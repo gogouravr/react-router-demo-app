8ebe5f38e857ba82375513449182eec1732bf8f6
gouravrajbit <gouravr244@gmail.com>
2024-11-07 21:01:36 +0530

    Add back Link route with relative path

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,16 @@
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
+
 
 export default function ProductDetails() {
     const { productId } = useParams();
     return (
-        <div>The ID of the product is {productId}</div>
+        <>
+            <div>The ID of the product is {productId}</div>
+            <span>
+                <Link to={'..'} relative="path">
+                    Go back!
+                </Link>
+            </span>
+        </>
     )
 }
